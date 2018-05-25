b548eb918235c15bc647f11b0f529f0b73da59ef
Andres <floresc.andres@intracitygeeks.org>
2018-05-25 17:04:08 -0400

    new API call for img url

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     $("#Lucky").click(function(){
         var query = $("#searchBar").val().replace(/\s+/g,"%20");
         var wikiCall = "https://en.wikipedia.org/w/api.php?origin=*&format=json&formatversion=2&action=query&generator=search&gsrnamespace=0&gsrsearch="+query+"&gsrlimit=10&prop=pageimages&pilimit=max";   
-        var wikiIMG = "https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=Image:Einstein_1921_by_F_Schmutzer_-_restoration.jpg&prop=imageinfo&iiprop=url&format=json&formatversion=2";
+        
         $.getJSON(wikiCall, wikiHunt);
-        // $.getJSON(wikiIMG, wikiPic);
+        
         
         function wikiHunt(wikiWhere){
             
@@ -15,12 +15,23 @@ $(document).ready(function() {
                 var wikiNewName = wikiName.replace(/\s+/g, "_");
                 var wikiPickMe = "https://en.wikipedia.org/wiki/"+wikiNewName;
                 var nameHTML = "<h3 class='PageName'>"+wikiName+"</h3>";
-                var wikiGO = "<a href='"+wikiPickMe+"'>"+nameHTML+"</a>";
+                var wikiGO = "<a class='pageLink' href='"+wikiPickMe+"'>"+nameHTML+headLogo+"</a>";
                 
                 $(".results").append(wikiGO);
+                
+                var headLogo = "<img src='https://en.m.wikipedia.org/wiki/Wikipedia_logo#/media/File%3AWikipedia-logo-v2.svg' alt='Logo'>";
+                var wikiPhoto = wikiWhere.query.pages[i].pageimage;
+                // var wikiIMG = "https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=Image:Einstein_1921_by_F_Schmutzer_-_restoration.jpg&prop=imageinfo&iiprop=url&format=json&formatversion=2"; 
+                // $.getJSON(wikiIMG, wikiPic);
+                
+                // if (wikiPhoto !== undefined){
+                //     function wikiPic(goGetIt){
+                //         var newPhoto =
+                //     }
+                // }
+                
                 console.log(wikiGO);
-            }
-        
+            };
         }
     });
 });
