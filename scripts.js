e21d0a4633841a42039b0c1fea2ddb5ecbf4137f
Andres <floresc.andres@intracitygeeks.org>
2018-05-25 16:35:53 -0400

    It grabs 10 results and makes them into links

--- scripts.js
+++ scripts.js
@@ -2,30 +2,31 @@ $(document).ready(function() {
 
     $("#Lucky").click(function(){
         var query = $("#searchBar").val().replace(/\s+/g,"%20");
-        // var wikiCall = "https://en.wikipedia.org/w/api.php?origin=*&format=json&formatversion=2&action=query&generator=search&gsrnamespace=0&gsrsearch="+query+"&gsrlimit=10&prop=pageimages&pilimit=max";   
-        var wikiCall = "https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=Image:Einstein_1921_by_F_Schmutzer_-_restoration.jpg&prop=imageinfo&iiprop=url&format=json&formatversion=2";
+        var wikiCall = "https://en.wikipedia.org/w/api.php?origin=*&format=json&formatversion=2&action=query&generator=search&gsrnamespace=0&gsrsearch="+query+"&gsrlimit=10&prop=pageimages&pilimit=max";   
+        var wikiIMG = "https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=Image:Einstein_1921_by_F_Schmutzer_-_restoration.jpg&prop=imageinfo&iiprop=url&format=json&formatversion=2";
         $.getJSON(wikiCall, wikiHunt);
-        
-        // var wikiFind = {
-        //     format: "json",
-        //     list: "search",
-        //     srsearch: query,
-        //     origin: "*",
-        //     action: "query",
-        //     srlimit: "10",
-        //     srprop: "images",
-        // };
-        
-        // var foundit = $.ajax({type: "GET", url: wikiCall, data: wikiFind});
+        // $.getJSON(wikiIMG, wikiPic);
         
         function wikiHunt(wikiWhere){
-        console.log(wikiWhere);
+            
+            
+            for(var i = 0; i < wikiWhere.query.pages.length; i++){
+                var wikiName = wikiWhere.query.pages[i].title;
+                var wikiNewName = wikiName.replace(/\s+/g, "_");
+                var wikiPickMe = "https://en.wikipedia.org/wiki/"+wikiNewName;
+                var nameHTML = "<h3 class='PageName'>"+wikiName+"</h3>";
+                var wikiGO = "<a href='"+wikiPickMe+"'>"+nameHTML+"</a>";
+                
+                $(".results").append(wikiGO);
+                console.log(wikiGO);
+            }
+        
         }
     });
 });
 
-// api.php?action=query&srsearch=morelike:Marie_Curie|radium&srprop=size&formatversion=2
+//wikiCall will run to wikipedia and find the associated pages to the search
+// var wikiCall = "https://en.wikipedia.org/w/api.php?origin=*&format=json&formatversion=2&action=query&generator=search&gsrnamespace=0&gsrsearch="+query+"&gsrlimit=10&prop=pageimages&pilimit=max"; 
 
-//this one works: "https://en.wikipedia.org/w/api.php?&action=query&origin=*&list=search&srsearch=title:"+query+"&srlimit=10&format=json&formatversion=2"
-
-//this one doesnt work with the images: https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&srsearch=title:"+query+"&srlimit=10&prop=pageimages&format=json&formatversion=2
+//wikiIMG will use the image parameter from the call to fin the URL of the image
+// var wikiIMG = "https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=Image:Einstein_1921_by_F_Schmutzer_-_restoration.jpg&prop=imageinfo&iiprop=url&format=json&formatversion=2";
